36572d3f599bb2f59bd9048cd618f640d78f7946
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2020-11-28 13:22:33 +0100

    add interactable Objects and renew background

---
+++ Assets/Script/InteractableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject commandMenu;
+    public UI_Manager uiManager;
+    
+    //Erster Frame des Objektes in der Szene
+    public void start()
+    {
+        commandMenu.active = false;
+    }
+    
+    //Diese Funktion wird jeden Frame aufgerufen
+    public void update()
+    {
+    
+    }
+    
+    public void OnMouseDown()
+    {
+        command.Menu.active = true;
+        commandMenu.transform.position = transform.position;
+
+        uiManager.activeIO = this;
+    }
+}
