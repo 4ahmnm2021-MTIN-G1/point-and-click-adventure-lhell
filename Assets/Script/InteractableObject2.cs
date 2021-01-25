ad49f5bfac8882f7251b4e80195835b3c2d6e901
HellLisa <lisahell@nbgum-lhell03-MacBook-Pro.local>
2021-01-25 08:49:14 +0100

    add second interactable script and render texture

---
+++ Assets/Script/InteractableObject2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class InteractableObject2 : MonoBehaviour
+{
+    public GameObject commandMenu;
+    public ui_Manager ui_Manager;
+    public Text dialogFenster;
+    
+    public bool condition;
+    public bool condition2;
+    public UnityEvent conditionMet;
+    public UnityEvent conditionNotMet;
+
+    public UnityEvent talk;
+    
+    
+    public void OnMouseDown2()
+    {
+        commandMenu.active = true;
+        commandMenu.transform.position = transform.position;
+
+        //dialogFenster.text = inspectText;
+    
+        //ui_Manager.activeIO = this;
+
+    }
+
+    public void Interact2()
+    {
+        if (condition && condition2)
+        {
+            conditionMet.Invoke();
+        }
+        else 
+        {
+            conditionNotMet.Invoke();
+        }
+    }
+
+     public void SetCondition(bool value)
+        {
+            condition = value;
+        }
+
+    public void SetCondition2(bool value)
+        {
+            condition2 = value;
+        }
+}
